69f4e94722ab07436fec77630f3fdf1b3d38b4cf
Nikolay Ivanov <Nikolay Ivanov>
2016-10-15 20:01:31 +0100

    working

--- terrainGen.js
+++ terrainGen.js
@@ -5,11 +5,16 @@ const max_length = 400;
 
 var cur_height, cur_length;
 var queue=[];
+var queue2 = [];
 var drawQueue = [];
 var max_jump = 200;
 var xcoord=0;
 var ycoord;
 var xlength;
+
+
+var somearray = []; //only for tasting
+var bla1, bla2; //only for testing
 
 function genFirstBlock(){
     //generate first lock with a number between 600 and 50
@@ -17,6 +22,7 @@ function genFirstBlock(){
     queue.push(cur_height);
     cur_length =  Math.floor((Math.random() * 250) + 150);
     queue.push(cur_length);
+    return queue;
     //console.log(cur_height);
 }
 
@@ -26,21 +32,22 @@ function genNextBlock(cur_height,cur_length){
     console.log("KURA NA HEIGHTA E : ", cur_height);
     var temp = Math.max((cur_height-200),200);
     cur_height = Math.floor((Math.random()*(750-temp)) + temp);
-    queue.push(cur_height);
+    queue2.push(cur_height);
     cur_length =  Math.floor((Math.random()*250) + 150);
     //cur_length = Math.floor((Math.random()*max_length-150)+min_length);
-    queue.push(cur_length);
+    queue2.push(cur_length);
+    return queue2;
     //console.log(queue);
 }
 
-genFirstBlock();
-console.log(queue);
+//genFirstBlock();
+//console.log(queue);
 for(var i  = 0; i < 5; i++){
     ycoord = queue.shift();
     xlength = queue.shift();
     drawQueue.push(ycoord);
     drawQueue.push(xlength);
-    genNextBlock(ycoord,xlength);
+    //genNextBlock(ycoord,xlength);
     //  console.log(cur_height,' ',cur_length);
 
 }
@@ -53,51 +60,3 @@ var terrainBlock;
 var background;
 var width = 1000;
 var height = 800;
-
-function init() {
-	canvas = document.getElementById("game_canvas");
-	width = canvas.width;
-	height = canvas.height;
-	ctx = canvas.getContext("2d");
-
-	// init background
-	//background = new Image();
-	//background.src = 'button.png';
-    terrainBlock = new Image();
-    terrainBlock.src = "block5.png";
-
-
-	return setInterval(main_loop, 30);
-}
-
-
-
-function drawTerrain() {
-	//ctx.drawImage(background,0,0);
-    //if(queue.)
-    //console.log(queue, ' PREDI ');
-
-    // var ycoord=queue.shift();
-    // var xlength=queue.shift();
-    var yc = drawQueue.shift();
-    var xl = drawQueue.shift();
-    // console.log(queue, ' QUEUE');
-    ctx.drawImage(terrainBlock,xcoord,yc,xlength,50);
-    // console.log(ycoord, " y COORD");
-    // console.log(xcoord, " x coord");
-    //ctx.drawImage(terrainBlock,xcoord,ycoord,xlength,50);
-    xcoord =xcoord + xlength;
-    //ctx.drawImage(terrainBlock,0,576,300,50);
-
-
-}
-
-function main_loop() {
-    // console.log(cur_height,' ',cur_length);
-
-	drawTerrain();
-}
-
-
-
-init();
