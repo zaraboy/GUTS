200a4e2393bb7ca8ee50fe7f9bc8b5a18bd3069e
PeshoGoshov <vladimirbardarski@abv.bg>
2016-10-15 05:31:30 +0100

    Бачка
    Бачка бе

--- startMenu.js
+++ startMenu.js
@@ -25,10 +25,12 @@ function init() {
 	// init cloud
 	startButton = new Image();
   title = new Image();
-  title.src = 'text.png'
+  title.src = 'title.png'
 	startButton.src = 'button.png';
   label = new Image();
-  label.src = 'ground4.png'
+  label.src = 'label.png'
+	testplat = new Image();
+	testplat.src = 'block5.png'
 	startButton.onload = function(){
 		buttonWidth = width/2;
     buttonHeight = height/2;
@@ -47,6 +49,7 @@ function init() {
 function draw() {
 	ctx.drawImage(background,0,0);
 	ctx.drawImage(startButton, width/3, height/3, width/3, height/3);
+	//ctx.drawImage(testplat, width/3, height/3, 150, 50);
   ctx.drawImage(title, width/6, height/7, 4*width/6, height/6);
   ctx.drawImage(label, width/6, 5*height/7, 4*width/6, height/12);
 }
